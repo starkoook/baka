5e62b19a8bfc446451211de3176c46f6bb8df1a6
starkoook <starkoook@users.noreply.github.com>
2026-06-05 00:01:44 +0800

    修复生产构建路径: renderer → dist/renderer

--- electron/main.js
+++ electron/main.js
@@ -60,7 +60,7 @@ function createWindow() {
     mainWindow.loadURL('http://localhost:5173')
     mainWindow.webContents.openDevTools({ mode: 'detach' })
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(join(__dirname, '../dist/renderer/index.html'))
   }
 }
 
